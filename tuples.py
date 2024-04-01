0fe99f22f4ed8d863a1bbaa95ec44450099362f4
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-04-01 11:08:11 -0500

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,22 @@
+''' 1: Tuple Mastery In Python
+Objective:
+The aim of this assignment is to deepen your understanding of tuples in Python, along with their interaction 
+with lists, dictionaries, and basic Python functionalities like functions, input handling, adn string formatting.
+By completing this assignment, you will enhance your skills in data structuring, manipulation, and application of tuples in practical scenarios.
+
+Task 1: Formatting Flight Itineraries
+    Create a Python function that takes a list of tuples as an argument. 
+    Each tuple contains information about a flight itinerary: (traveler_name, origin, destination). 
+    The function should format and return a string that lists each itinerary.
+    For example, if the input is [("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco")] 
+    
+    The output should be formatted as:
+
+        "Itinerary 1: Alice - From New York to London
+        Itinerary 2: Bob - From Tokyo to San Francisco"    
+
+'''
+
+def display_flight_information():
+
+    traveler = input("Enter the Traveler's name: ")
